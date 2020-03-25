72745a9647c8a841b0d841a1f97677b05fc1fcbc
Agoetz2 <aj3goetz@gmail.com>
2020-03-25 15:30:51 -0400

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DecklistComponent } from './decklist/decklist.component';
+import { DeckDetailsComponent } from './deck-details/deck-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, DecklistComponent ],
+  declarations: [ AppComponent, DecklistComponent, DeckDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
---
+++ src/app/deck-details/deck-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { Deck } from '../decks';
+
+
+
+@Component({
+  selector: 'app-deck-details',
+  templateUrl: './deck-details.component.html',
+  styleUrls: ['./deck-details.component.css']
+})
+
+
+export class DeckDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
